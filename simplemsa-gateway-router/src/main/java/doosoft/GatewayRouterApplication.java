99ce098376f403dff0f0737c5bc3d97bde832749
kimjd72 <kimjd72@gmail.com>
2020-02-03 16:29:40 +0900

    application registration on eureka

--- simplemsa-gateway-router/src/main/java/doosoft/GatewayRouterApplication.java
+++ simplemsa-gateway-router/src/main/java/doosoft/GatewayRouterApplication.java
@@ -6,9 +6,11 @@ import doosoft.filter.GatewayPreFilter;
 import doosoft.filter.GatewayRouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayRouterApplication {
